38fff7c562fc9ba46f2cbf8972fc7f29e09c075c
Sindre Andreassen <sindrema@gmail.com>
2022-02-19 00:08:24 +0100

    first commit

---
+++ WowChatGrabber/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ResponseChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("test");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("test2");
+            this.buttonstart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Xlabel = new System.Windows.Forms.Label();
+            this.Ylabel = new System.Windows.Forms.Label();
+            this.XValue = new System.Windows.Forms.Label();
+            this.YValue = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // buttonstart
+            // 
+            this.buttonstart.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonstart.Location = new System.Drawing.Point(215, 6);
+            this.buttonstart.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonstart.Name = "buttonstart";
+            this.buttonstart.Size = new System.Drawing.Size(141, 41);
+            this.buttonstart.TabIndex = 13;
+            this.buttonstart.Text = "Select area";
+            this.buttonstart.UseVisualStyleBackColor = true;
+            this.buttonstart.Click += new System.EventHandler(this.buttonstart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Selected area";
+            // 
+            // Xlabel
+            // 
+            this.Xlabel.AutoSize = true;
+            this.Xlabel.Location = new System.Drawing.Point(12, 22);
+            this.Xlabel.Name = "Xlabel";
+            this.Xlabel.Size = new System.Drawing.Size(17, 13);
+            this.Xlabel.TabIndex = 15;
+            this.Xlabel.Text = "X:";
+            // 
+            // Ylabel
+            // 
+            this.Ylabel.AutoSize = true;
+            this.Ylabel.Location = new System.Drawing.Point(104, 22);
+            this.Ylabel.Name = "Ylabel";
+            this.Ylabel.Size = new System.Drawing.Size(17, 13);
+            this.Ylabel.TabIndex = 16;
+            this.Ylabel.Text = "Y:";
+            // 
+            // XValue
+            // 
+            this.XValue.AutoSize = true;
+            this.XValue.Location = new System.Drawing.Point(35, 22);
+            this.XValue.Name = "XValue";
+            this.XValue.Size = new System.Drawing.Size(0, 13);
+            this.XValue.TabIndex = 17;
+            // 
+            // YValue
+            // 
+            this.YValue.AutoSize = true;
+            this.YValue.Location = new System.Drawing.Point(121, 22);
+            this.YValue.Name = "YValue";
+            this.YValue.Size = new System.Drawing.Size(0, 13);
+            this.YValue.TabIndex = 18;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(12, 50);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(344, 159);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 221);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.YValue);
+            this.Controls.Add(this.XValue);
+            this.Controls.Add(this.Ylabel);
+            this.Controls.Add(this.Xlabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonstart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "WowChatGrabber";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonstart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Xlabel;
+        private System.Windows.Forms.Label Ylabel;
+        private System.Windows.Forms.Label XValue;
+        private System.Windows.Forms.Label YValue;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ WowChatGrabber/Form1.cs
@@ -0,0 +1,293 @@
+﻿using DeepCopy;
+using howto_bitmap32;
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using System;
+using IronOcr;
+
+namespace ResponseChecker
+{
+    public partial class Form1 : Form
+    {
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        private Bitmap TakeScreenshot()
+        {
+            var size = new Size(SelectedArea.Width, SelectedArea.Height);
+            var bmpScreenshot = new Bitmap(SelectedArea.Width,
+                               SelectedArea.Height, PixelFormat.Format32bppArgb);
+            
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(SelectedArea.Left, SelectedArea.Top, 0, 0, size);
+            ConvertBitmapToGrayscale(bmpScreenshot, false);
+            return bmpScreenshot;
+        }
+
+
+
+        public static Rectangle SelectedArea;
+
+
+        HubConnection connection;
+        public Bitmap ConvertToBitmap(string fileName)
+        {
+            Bitmap bitmap;
+            using (Stream bmpStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            {
+                Image image = Image.FromStream(bmpStream);
+
+                bitmap = new Bitmap(image);
+
+            }
+            return bitmap;
+        }
+
+        tessnet2.Tesseract ocr;
+        //ocr.SetVariable("tessedit_char_whitelist", "0123456789abcdefghijkLmnOpqrstuvwxyz.,$-/#&=()\"'\\:?"); // Accepted characters
+        
+        public Form1()
+        {
+            ocr = new tessnet2.Tesseract();
+            ocr.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,$-/\\#&=()\"':?"); // Accepted characters
+            ocr.Init(@"C:\Programmer\WowChatGrabber\WowChatGrabber\Content\tessdata", "eng", false); // Directory of your tessdata folder
+
+            InitializeComponent();
+            if (File.Exists(LastSelectedAreaFile)) SetRegion(JsonConvert.DeserializeObject<Rectangle>(File.ReadAllText(LastSelectedAreaFile)), true);
+            connection = new HubConnectionBuilder()
+              .WithUrl("http://192.168.10.111:50486/JukyHub")
+              .Build();
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+            connection.StartAsync();
+
+            Timer watchTimer = new Timer();
+            watchTimer.Interval = 250;
+            watchTimer.Tick += (l, a) =>
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                int Xvalue = 0;
+                int Yvalue = 0;
+                if (int.TryParse(XValue.Text, out Xvalue) && int.TryParse(YValue.Text, out Yvalue))
+                {
+                    if (IsGameRunning() || true)
+                    {
+                        var screenshot = TakeScreenshot();
+                        //var screenshot = ConvertToBitmap(@"grey.jpg");
+
+                        var firstPix = screenshot.GetPixel(1, 1);
+                        // if is dark screenshot
+                        if (firstPix.A == 255 && firstPix.R == 0 && firstPix.G == 0 && firstPix.B == 0)
+                        {
+                            //SaveScreenshot(screenshot, "testttt");
+                            var text = GetTextFromImage2(screenshot);//.Replace("\n", "");
+
+                            if (text.Contains("=") && text.Contains("#"))
+                            {
+                                var split = text.Split('=');
+                                var userAndTimestamp = split.FirstOrDefault().Replace(" ", "");
+                                var userAndTimestampSplit = userAndTimestamp.Split('#');
+
+                                var user = userAndTimestampSplit.LastOrDefault();
+                                var time = userAndTimestampSplit.FirstOrDefault();
+
+                                var message = text.Replace(split.FirstOrDefault() + "=", "");
+                                var TimeAndUserAndMsg = $"[{time}]{user}: {message}";
+
+                                bool exists = false;
+                                foreach (ListViewItem item in listView1.Items)
+                                {
+                                    if (item.Text.ToLower() == TimeAndUserAndMsg.ToLower())
+                                    {
+                                        exists = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!exists)
+                                {
+                                    PostMessage(284843089384701952, message, user);
+                                    //SaveScreenshot(GreyScreenshot, "grey");
+                                    listView1.Items.Add(TimeAndUserAndMsg);
+                                    if (listView1.Items.Count > 9)
+                                    {
+                                        listView1.Items.RemoveAt(0);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                sw.Stop();
+                Console.WriteLine(sw.ElapsedMilliseconds);
+            };
+            watchTimer.Start();
+        }
+
+        private async void PostMessage(long discord_id, string message, string user)
+        {
+            if(connection.State != HubConnectionState.Connected)
+            {
+                connection.StartAsync();
+            }
+            
+            await connection.InvokeAsync("PostMessage", discord_id, message.ToLower(), user, "THisiSDEadminKey21");
+        }
+
+        private void GetLinesFromChat(string text)
+        {
+            if (text.Contains("[") && text.Contains("]") && text.Contains(":"))
+            {
+
+            }
+        }
+        private void ConvertBitmapToGrayscale(Bitmap bm, bool use_average)
+        {
+            // Make a Bitmap24 object.
+            Bitmap32 bm32 = new Bitmap32(bm);
+
+            // Lock the bitmap.
+            bm32.LockBitmap();
+
+            // Process the pixels.
+            for (int x = 0; x < bm.Width; x++)
+            {
+                for (int y = 0; y < bm.Height; y++)
+                {
+                    byte r = bm32.GetRed(x, y);
+                    byte g = bm32.GetGreen(x, y);
+                    byte b = bm32.GetBlue(x, y);
+                    byte gray = (use_average ?
+                        (byte)((r + g + b) / 3) :
+                        (byte)(0.3 * r + 0.5 * g + 0.2 * b));
+                    bm32.SetPixel(x, y, gray, gray, gray, 255);
+                }
+            }
+
+            // Unlock the bitmap.
+            bm32.UnlockBitmap();
+        }
+        private void ConvertToGreyImage(Bitmap bitmap)
+        {
+            Color c;
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    c = bitmap.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(c.R, 0, 0);
+                    bitmap.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        private string GetTextFromImage2(Bitmap screenshot)
+        {
+            
+            var engine = new TesseractEngine(@"./tessdata", "eng");
+            Page page = engine.Process(screenshot, PageSegMode.SingleLine);
+            string result = page.GetText();
+            return result;
+            
+
+            /*
+            var Ocr = new AutoOcr();
+            var Result = Ocr.Read(screenshot);
+            return (Result.Text);
+            */
+            return null;
+        }
+        private string GetTextFromImage1(Bitmap screenshot)
+        {
+            try
+            {
+                
+                List<tessnet2.Word> result = ocr.DoOCR(screenshot, System.Drawing.Rectangle.Empty);
+                var text = string.Join(" ", result.Select(x => x.Text));
+                return text;
+
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        private void SaveScreenshot(Bitmap screenshot, string name)
+        {
+            screenshot.Save(name + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        public string GameProcessName = "WowB";
+        public bool IsGameRunning()
+        {
+            return Process.GetProcessesByName(GameProcessName).Count() != 0;
+        }
+        public Process GetGameProcess()
+        {
+            return Process.GetProcessesByName(GameProcessName).FirstOrDefault();
+        }
+
+        public void Post(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(url);
+        }
+        public RegionSelector regionSelector;
+        private void buttonstart_Click(object sender, EventArgs e)
+        {
+            regionSelector = new RegionSelector();
+            regionSelector.FormClosed += Re_FormClosed;
+        }
+        public void SetRegion(Rectangle rect, bool DontSave = false)
+        {
+            SelectedArea = rect;
+            XValue.Text = rect.X.ToString();
+            YValue.Text = rect.Y.ToString();
+            if (!DontSave)
+            {
+                File.WriteAllText(LastSelectedAreaFile, JsonConvert.SerializeObject(SelectedArea));
+            }
+
+        }
+        private void Re_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SetRegion(regionSelector.rect);
+        }
+        private string LastSelectedAreaFile = "LastSelectedArea.json";
+    }
+}
